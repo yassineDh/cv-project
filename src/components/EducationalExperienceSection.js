e8f7e750df8bcceefec6338c6f9ea34ed546ba81
yassineDh <yassine.dahane96@gmail.com>
2021-03-23 09:25:25 +0100

    commit

--- src/components/EducationalExperienceSection.js
+++ src/components/EducationalExperienceSection.js
@@ -16,37 +16,50 @@ class EducationalExperienceSection extends Component {
       educationalExperiences: [],
     };
 
-    // this.submitHandler = this.submitHandler.bind(this);
-    // this.changeHandler = this.changeHandler.bind(this);
-    // this.updateHandler = this.updateHandler.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+    this.changeHandler = this.changeHandler.bind(this);
+    this.updateHandler = this.updateHandler.bind(this);
     this.addExperience = this.addExperience.bind(this);
   }
 
-  // submitHandler(e) {
-  //   e.preventDefault();
-  //   console.log(this.state);
-  //   // this.setState({
-  //   //   educationalExperiences[i].isComplete: true;
-  //   // });
-  //   this.setState((prevState)=>({
-  //     prevState.educationalExperiences[i].isComplete: true;
-  //   }));
-  // }
+  submitHandler(e) {
+    e.preventDefault();
+    // console.log(this.state);
+    // this.setState({
+    //   educationalExperiences[i].isComplete: true;
+    // });
 
-  // changeHandler(e) {
-  //   const name = e.target.name;
-  //   const value = e.target.value;
-  //   this.setState({
-  //     [name]: value,
-  //   });
-  // }
-  // updateHandler(e) {
-  //   e.preventDefault();
-  //   console.log("inside update");
-  //   this.setState({
-  //     isComplete: false,
-  //   });
-  // }
+    let completedArray = this.state.educationalExperiences.map((element) => {
+      let newObj = Object.assign({}, element, { isComplete: true });
+      return newObj;
+    });
+    this.setState(
+      { educationalExperiences: completedArray },
+      console.log(this.state)
+    );
+  }
+
+  changeHandler(e,i) {
+    let name = e.target.name;
+    let value = e.target.value;
+    let newArray = this.state.educationalExperiences.map((element,index)=>{
+      if(index == i){
+        element[name]= value
+      }
+
+      return element;
+    })
+    this.setState({
+      educationalExperiences:newArray
+    });
+  }
+  updateHandler(e) {
+    e.preventDefault();
+    console.log("inside update");
+    this.setState({
+      isComplete: false,
+    });
+  }
 
   addExperience() {
     // let prevArr = this.state.educationalExperiences.slice(0);
@@ -61,6 +74,7 @@ class EducationalExperienceSection extends Component {
     return (
       <React.Fragment>
         <button onClick={this.addExperience}>Add educational experience</button>
+        <button onClick={this.submitHandler}>test</button>
         {/* <EducationalExperience update={this.updateHandler} submit={this.submitHandler} change={this.changeHandler} isComplete={this.state.isComplete}/> */}
         {/* {this.state.educationalExperiences.map((element,index) =><EducationalExperience update={this.updateHandler} submit={this.submitHandler} change={this.changeHandler} isComplete={element.isComplete}/>  )} */}
       </React.Fragment>
